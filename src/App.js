3b06c5f385dbdbda9e5c2a4fc9b060a23427d57c
khargr14 <kay_hargrove@yahoo.com>
2021-04-26 22:45:56 -0400

    add packages

--- src/App.js
+++ src/App.js
@@ -3,15 +3,6 @@ import React from 'react';
 
 class App extends React.Component{
 
-componentDidMount() {
-  fetch('http://localhost:3000/api/v1/businesses/11/contacteds', {
-    method: 'GET'
-  })
-  .then(response => response.json())
-  .then(data => console.log(data))
-}
-
-
   render(){
     return(
       <div className="App">
