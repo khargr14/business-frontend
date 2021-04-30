2ed5fd33dcd38acb65522f905ec9c3f3f500463d
khargr14 <kay_hargrove@yahoo.com>
2021-04-29 20:57:47 -0400

    creating routes, switch and correcting show page

--- src/components/Business.js
+++ src/components/Business.js
@@ -1,16 +1,23 @@
 import React from 'react'
+//import {Redirect} from 'react-router-dom'
+
 
 
 const Business = (props) => {
-
+ 
 console.log(props)
-//let business=props.businesses[props.match.params.id-1];
-let business=props.businesses[props.match.params.id-1];
-console.log(business,"data")
+
+let business = props.businesses.filter(business => business.id == props.match.params.id)[0]
+//let business = props.businesses[props.match.params.id-1];
+// let business=props.businesses[props.match.params.id-1];
+// console.log(business,"data")
 return (
-    <li>
-       {business?business.name:null} - {business?business.number:null} - {business?business.email:null} - {business?business.industry:null}       
-    </li>
+    <h3>
+       {/* {business ? null: <Redirect to='/businesses'/>} */}
+       {business ? business.name : null} 
+       
+       <h5> {business?business.number:null} - {business?business.email:null} - {business?business.industry:null}  </h5>     
+    </h3>
 
 
 )
--- src/components/Businesses.js
+++ src/components/Businesses.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Route, Link} from 'react-router-dom'
 import Business from './Business'
 
 
@@ -6,14 +7,19 @@ import Business from './Business'
 
 
  const Businesses = (props) => {
-                                                                 //   :name, :number, :email, :industry, :contacted_id
+                           //   :name, :number, :email, :industry, :contacted_id
     return(
-        <div>
+    <div>
          {props.businesses.map(business => 
-         <div key={business.id}><Business business={business}/></div>)}
-        </div>
+      <li key={business.id}>
+          <Link to={`/businesses/${business.id}`}>{business.name} - {business.number}- {business.email}- {business.industry}</Link>
+      </li> )}
+
+    </div>
+       
+    
     )
+ }
  
-}
 
  export default Businesses
